3ff0444a4942539529fb3c5d31f1593e64b97e08
flex-akin <akintolafelix2121@gmail.com>
2025-05-01 09:32:21 +0100

    feat: using prompt class and message in spring ai chatclient

--- src/main/java/com/spring_ai_tut/playg/ChatController.java
+++ src/main/java/com/spring_ai_tut/playg/ChatController.java
@@ -1,5 +1,8 @@
 package com.spring_ai_tut.playg;
 
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,11 +10,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ChatController {
 
+    private final ChatClient chatClient;
+
+    public ChatController(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder.build();
+    }
+
     @GetMapping()
     public String generate(
-            @RequestParam()
+            @RequestParam(value = "message", defaultValue = "tell ne about arsenal") String message
     ){
+        return this.chatClient.prompt().user(message).call().content();
+    }
 
-        return "Hello World!";
+    @GetMapping("/prompt")
+    public String prompt(){
+        return this.chatClient.prompt(new Prompt("tell ma about chelsea")).call().content();
     }
 }
